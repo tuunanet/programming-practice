7af7b536fd54b635465de69a0a0e5f426cb08b16
Tuomo Tuunanen <tuomo.tuunanen@silo.ai>
2022-09-18 19:41:01 +0300

    Create random data.

--- language/python/multiprocessing/shared-memory/example1.py
+++ language/python/multiprocessing/shared-memory/example1.py
@@ -1,5 +1,14 @@
 from multiprocessing import Pool, Process, Array
+import numpy as np
 
 if __name__ == '__main__':
-    with Pool(4):
-        pass
+    #1
+    print("# 1. Create some random NumPy array data")
+    m = 100
+    n = 100
+    X = np.random.randint(0, 1000, size=(m, n), dtype=int)
+    print(f'--- Created random integer array X with shape={X.shape}, '
+    'and elements bound between [0, 1000]')
+
+    #2
+    print("# 2. Create shared memory array")
